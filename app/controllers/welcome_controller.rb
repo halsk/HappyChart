4b081d94f87e53e533dedc8e7153d3d750e8fda1
Hal Seki <hal@plants-web.jp>
2013-10-22 23:49:04 +0900

    reading from db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
   end
   def home
+    @categories = Category.includes(:questions).where("form_id = 2").order(:order_num).references(:questions)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   belongs_to :form
+  has_many :questions
 
 end
