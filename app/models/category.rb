af9315de55b4d73d21b6657f4ace0a7af54901e2
Hal Seki <hal@plants-web.jp>
2013-10-23 00:26:00 +0900

    added home

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
   belongs_to :form
   has_many :questions
 
+  def icon_name
+    sprintf("icon%02d.png", icon_id)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :category
+  def field_name
+    sprintf "q-%d-%d", self.category.form.id, self.id
+  end
 end
