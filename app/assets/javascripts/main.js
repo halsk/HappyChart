87a55fdb8c670290d76a18926cd5e27438f16625
Hal Seki <hal@plants-web.jp>
2013-10-29 03:48:10 +0900

    don't use fb location if it has no prefecture name

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,14 @@
+var PREF = ["北海道","青森県","岩手県","宮城県","秋田県" ,
+    "山形県","福島県","茨城県","栃木県","群馬県",
+    "埼玉県","千葉県","東京都","神奈川県", "新潟県",
+    "富山県","石川県","福井県","山梨県","長野県",
+    "岐阜県","静岡県","愛知県","三重県","滋賀県" ,
+    "京都府","大阪府","兵庫県","奈良県","和歌山県",
+    "鳥取県","島根県","岡山県","広島県","山口県" ,
+    "徳島県","香川県","愛媛県","高知県","福岡県",
+    "佐賀県","長崎県","熊本県","大分県","宮崎県" ,
+    "鹿児島県", "沖縄県" ];
+
 window.fbAsyncInit = function () {
     FB.init({
   appId : "439649252820957",
@@ -32,6 +43,15 @@ var fblogin = function(){
   }});
 };//fblogin
 
+var prefCheck = function(location){
+  for (i = 0;i < PREF.length; i++){
+    if (location.indexOf(PREF[i]) == 0){
+      return true;
+    }
+  }
+  return false;
+}
+
 var getdata = function (bb){
     //     console.log("getdata"+JSON.stringify(bb));
     //     alert("resp:"+JSON.stringify(bb));
@@ -41,8 +61,12 @@ var getdata = function (bb){
   $.cookie("username",resp.username);
   $.cookie("name",resp.name);
   $.cookie("birthday",resp.birthday);
-  $.cookie("location",resp.location.name);
-  $.cookie("hometown",resp.hometown.name);
+  if (prefCheck(resp.location.name)){
+    $.cookie("location",resp.location.name);
+  }
+  if (prefCheck(resp.hometown.name)){
+    $.cookie("hometown",resp.hometown.name);
+  }
   //       console.log("cookie:"+JSON.stringify($.cookie()));
   document.location="/welcome/home";
     });
